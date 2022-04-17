7dbe94476f291e6851db7458699d3fd2853b9896
= <=>
2022-04-16 21:09:33 -0300

    Finaliza exercicio

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿double numerador, denominador, resultadoDivisao;
+
+Console.Write("Digite o numerador....:");
+numerador = Convert.ToDouble (Console.ReadLine());
+
+Console.Write("Digite o denominador..:");
+denominador = Convert.ToDouble (Console.ReadLine());
+
+if ( denominador == 0)
+{
+    Console.WriteLine("Não é possível dividir por zero.");
+}
+
+else  
+{
+    resultadoDivisao = numerador / denominador ;
+    Console.WriteLine($"{numerador:N1} divido por {denominador:N1} é {resultadoDivisao:N1}");
+}
